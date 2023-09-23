b8db9054b0a0cbb92c67e8719abb22acc99d49f3
nekembe <nekembeb@gmail.com>
2023-09-23 20:51:36 +0100

    first commit

---
+++ collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,22 @@
+"""Script contains STEP function that proves the Collatz Conjecture"""
+
+def steps(number):
+    """This function returns the number of times it takes for a number to be divided timm it reaches 1"""
+    
+    if number <= 0:
+        raise ValueError("Invalid Number. Number is below 1")
+    else:
+        times = 0
+
+        while number != 1:
+
+            result = 0
+            if number % 2 == 0:
+                result = number/2
+                times += 1
+            else:
+                result = (3 * number) + 1
+                times += 1
+            number = result
+
+    return times
