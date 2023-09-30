bb5ce691a416749c1549309479950d20158aa70e
nekembe <nekembeb@gmail.com>
2023-09-30 19:16:20 +0100

    initial commit

---
+++ armstrong-numbers/armstrong_numbers.py
@@ -0,0 +1,15 @@
+def is_armstrong_number(number:int):
+    """
+    :param number - integer to be verified
+
+    Determines whether a number is an Armstrong number
+    """
+    total = 0
+
+    for i in str(number):
+        total += int(i) ** len(str(number))
+
+    return total == number
+
+
+is_armstrong_number(9)
