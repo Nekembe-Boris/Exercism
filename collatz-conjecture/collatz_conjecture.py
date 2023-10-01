dc358c6612bba7fc06c614abdbfc76232690e1b8
nekembe <nekembeb@gmail.com>
2023-10-01 10:56:02 +0100

    pylint refactor

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,26 +1,23 @@
 """Script contains STEP function that proves the Collatz Conjecture"""
 
 def steps(number):
-    """This function returns the number of times it takes for a number to be divided timm it reaches 1"""
-    
+    """This function returns the number of times it takes for a number to be divided to reach 1"""
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
-    else:
-        times = 0
+    times = 0
+    while number != 1:
 
-        while number != 1:
-
-            result = 0
-            if number % 2 == 0:
-                result = number/2
-                times += 1
-            else:
-                result = (3 * number) + 1
-                times += 1
-            number = result
+        result = 0
+        if number % 2 == 0:
+            result = number/2
+            times += 1
+        else:
+            result = (3 * number) + 1
+            times += 1
+        number = result
 
     return times
 
 
-me = steps(12)
-print(me)
+ME_UP = steps(12)
+print(ME_UP)
