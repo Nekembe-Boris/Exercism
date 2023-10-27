5177b96b8315d0dec0d4c1e7936d7225dfd076ee
nekembe <nekembeb@gmail.com>
2023-10-27 22:12:27 +0100

    final code

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -2,3 +2,7 @@ def is_pangram(sentence:str):
     if (sen_letters := len([letter for letter in set(sentence.lower()) if letter.isalpha()])) == 26:
         return True
     return False
+
+
+me = [ 2,1,2,1,2,1,3]
+print(set(me))
