6d4d5a0717312aa8f42e43a0e7268bc74134a5eb
nekembe <nekembeb@gmail.com>
2023-11-20 22:14:18 +0100

    final submission

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,24 +1,20 @@
-vowels = ['a', 'e', 'i', 'o', 'u', 'xr', 'yt']
+VOWELS = {"a", "e", "i", "o", "u"}
+VOWELS_Y = {"a", "e", "i", "o", "u", "y"}
+SPECIALS = {"xr", "yt"}
 
 
-##UNFINISHED
 def translate(text):
-    for txt in text.split():
-        if txt[0] in vowels or txt[:2] in vowels:
-            return f'{txt}ay'
-        if text[0] == 'y':
-            return f'{txt[1:]}yay'
-        if txt[:2] == 'qu':
-            return f'{txt[2:]}quay'
-        if txt[0] not in vowels:
-            if txt[1:3] == 'qu':
-                return f'{txt[3:]}{txt[:3]}ay'
-            if txt[1] not in vowels:
-                for i, letter in enumerate(txt):
-                    if letter in vowels:
-                        return f'{txt[i:]}{txt[:i]}ay'
-            if 'y' in txt and txt.index('y') != 0:
-                for i, letter in enumerate(txt):
-                    if letter == 'y':
-                        return f'{txt[i:]}{txt[:i]}ay'
-            return f'{txt[1:]}{txt[0]}ay'
+    piggyfied = []
+
+    for word in text.split():
+        if word[0] in VOWELS or word[0:2] in SPECIALS:
+            piggyfied.append(word + "ay")
+            continue
+
+        for pos in range(1, len(word)):
+            if word[pos] in VOWELS_Y:
+                pos += 1 if word[pos] == 'u' and word[pos - 1] == "q" else 0
+                piggyfied.append(word[pos:] + word[:pos] + "ay")
+                break
+
+    return " ".join(piggyfied)
